e0c07c5250d1006ea1bb6901a5e9e31f4c3400a2
najibullahzadran <132771127+najibullahzadran@users.noreply.github.com>
2023-05-24 00:14:09 +0430

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -7,3 +7,21 @@ if MAFIA=="32bit":
 elif MAFIA=="64bit":
     #print('Command is in update wait we will fix it soon !')
     __import__("MAFIA")
+  .88b  d88.   .d8b.  d888888b  d888888b   .b8d.
+  88'YbdP'88  d8'  8b 88'         '88'    d8' '8b
+  88  88  88  88ooo88 88ooo.       88     88ooo88  
+  88  88  88  88~~~88 88~~~        88     88~~~88
+  88  88  88  88   88 88          .88.    88   88
+  YP  YP  YP  YP   YP YP        Y888888P  YP   YP ------------‐------------------‐----------------------
+ Author      :   Najibullah Zadran 
+ Facebook    :   Najibullah Zadran 
+ PUBG Mobile :   PEDROkpf
+ Status      :   Paid
+ Version     :   8.4 ------------------------------------------------------
+ Total accounts :2500
+ Selected code  :453
+ Process running in the background ------------------------------------------------------
+ [MAFIA-OK] 100088229585578 |
+ [MAFIA-OK] 100005678825633 |
+ [MAFIA-OK] 100014281926525 |
+ [MAFIA-XD]  88|  OK:3
